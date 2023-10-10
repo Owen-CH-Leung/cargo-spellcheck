75061122c745e9f2fd1a6058b0e82827414bd222
Bernhard Schuster <bernhard@ahoi.io>
2023-10-10 23:05:06 +0200

    ignore math

--- doc-chunks/src/markdown.rs
+++ doc-chunks/src/markdown.rs
@@ -154,7 +154,8 @@ impl<'a> PlainOverlay<'a> {
             Options::ENABLE_TABLES
                 | Options::ENABLE_FOOTNOTES
                 | Options::ENABLE_STRIKETHROUGH
-                | Options::ENABLE_TASKLISTS,
+                | Options::ENABLE_TASKLISTS
+                | Options::ENABLE_MATH,
             Some(broken_link_handler),
         );
 
@@ -201,6 +202,11 @@ impl<'a> PlainOverlay<'a> {
             };
 
             match event {
+                Event::Math(_dollas, _content) => {
+                    // skip math content
+                    dbg!(_dollas);
+                    dbg!(_content);
+                }
                 Event::Start(tag) => match tag {
                     Tag::Table(_alignments) => {
                         skip_table_text = true;
